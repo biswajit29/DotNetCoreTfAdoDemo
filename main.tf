018f8159c381456267bf012cdb43ed1c8aaed2e3
biswajit kar <bkar5@asda.co.uk>
2021-12-25 23:39:01 +0000

    adding tf file for creating RG and ACI

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.31.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "bktf_test" {
+    name = "bktftestrg"
+    location = "uksouth"
+}
+
+resource "azurerm_container_group" "bktf_cg" {
+    name = "dotnetcoreapi"
+    location = azurerm_resource_group.bktf_test.location
+    resource_group_name = azurerm_resource_group.bktf_test.name
+    ip_address_type = "public"
+    dns_name_label = "bktfapidns"
+    os_type = "Linux"
+    container {
+        name = "weatherapi"
+        image = "bismsit29/weatherapi"
+        cpu  = "1" 
+        memory = "1"
+        ports {
+            port = 80
+            protocol = "TCP"
+        }
+    }
+}
